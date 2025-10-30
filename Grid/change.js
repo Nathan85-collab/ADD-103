b80180ce7906c664fc876711f8299c5b497ef1bb
Nathan85-collab <nkinney@students.mchenry.edu>
2025-10-29 20:51:39 -0500

    GRID

---
+++ Grid/change.js
@@ -0,0 +1,19 @@
+function change_1() {
+  document.getElementById("image").src = "images/after/cheetah.jpg";
+}
+
+function change_2() {
+  document.getElementById("image").src = "images/after/cherry.jpg";
+}
+
+function change_3() {
+  document.getElementById("image").src = "images/after/fog.jpg";
+}
+
+function change_4() {
+  document.getElementById("image").src = "images/after/mario.jpg";
+}
+
+function change_5() {
+  document.getElementById("image").src = "images/after/rainbow.jpeg";
+}
